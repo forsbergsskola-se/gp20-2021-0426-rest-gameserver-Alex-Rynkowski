3d0fc65a9d6d5bf6cb46a2a936493df5efb1e8bb
Alex <Alex.rynkowski90@gmail.com>
2021-05-06 10:15:06 +0200

    step one complete

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Text.Json;
+using System.Threading.Tasks;
 
+//Assignment 3:
+//https://github.com/marczaku/GP20-2021-0426-Rest-Gameserver/blob/main/assignments/assignment3.md
 namespace LameScooter
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(args[0]);
         }
     }
 }
