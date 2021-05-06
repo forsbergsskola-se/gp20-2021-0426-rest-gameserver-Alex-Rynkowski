578e42330b08d9bb5b946729bf0998c9a35198e9
Alex <Alex.rynkowski90@gmail.com>
2021-05-06 19:46:19 +0200

    fix deprecated assignment

---
+++ LameScooter/DeprecatedLameScooterRental.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LameScooter{
+    public class DeprecatedLameScooterRental : ILameScooterRental{
+        async Task<Dictionary<string, int>> ScooterDataList(){
+            var dataList = new Dictionary<string, int>();
+            var txtFile = await Utilities.ReadFromFile.ReadFile("scooters.txt");
+            var splitText = txtFile.Split(" : ");
+            for (var i = 0; i < splitText.Length; i += 2){
+                var nr = splitText[i + 1];
+                var str = "";
+                for (var j = 0; j < nr.Length; j++){
+                    if (!char.IsDigit(nr[j])) continue;
+                    str += nr[j];
+                    j++;
+                }
+                dataList[splitText[i]] = Convert.ToInt32(str);
+            }
+
+            return dataList;
+        }
+
+        public async Task<int> GetScooterCountInStation(string stationName){
+            var dataList = await ScooterDataList();
+            return dataList[stationName];
+        }
+    }
+}
--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -4,40 +4,68 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Driver;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Attributes;
 
 //Assignment 3:
 //https://github.com/marczaku/GP20-2021-0426-Rest-Gameserver/blob/main/assignments/assignment3.md
 namespace LameScooter{
     class Program{
         static async Task Main(string[] args){
+            try{
+                switch (args[1]){
+                    case "offline":
+                        ILameScooterRental lameScooterRental = new OfflineLameScooterRental();
+                        var offlineTimeCount = await lameScooterRental.GetScooterCountInStation(args[0]);
+                        Console.WriteLine($"offline____Number of available scooters in {args[0]}: {offlineTimeCount}");
+                        break;
+                    case "deprecated":
+                        ILameScooterRental deprecatedScooters = new DeprecatedLameScooterRental();
+                        var deprecatedCount = await deprecatedScooters.GetScooterCountInStation(args[0]);
+                        Console.WriteLine(
+                            $"deprecated____Number of available scooters in {args[0]}: {deprecatedCount}");
+                        break;
+                    case "realtime":
+                        ILameScooterRental realTimeScooters = new RealTimeLameScooterRental();
+                        var realTimeCount = await realTimeScooters.GetScooterCountInStation(args[0]);
+                        Console.WriteLine($"real time____Number of available scooters in {args[0]}: {realTimeCount}");
+                        break;
+                    case "mongodb":
+                        await MongoDb(args[0]);
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid argument");
+                }
+            }
+            catch (IndexOutOfRangeException){
+                Console.WriteLine("Argument 2 has to be: \"offline\", \"deprecated\" or \"realtime\"");
+                throw;
+            }
+        }
+
+        static async Task MongoDb(string location){
             var client =
                 new MongoClient(
                     "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false");
 
             var database = client.GetDatabase("LameScooters");
             var collection = database.GetCollection<BsonDocument>("inventory");
+            var inventoryList = await collection.Find(new BsonDocument()).ToListAsync();
 
-            var tmp = await collection.Find(new BsonDocument()).ToListAsync();
+            var idsDictionary = new Dictionary<BsonValue, Dictionary<string, BsonValue>>();
 
-            Dictionary<BsonValue, Dictionary<string, BsonValue>> newDic =
-                new Dictionary<BsonValue, Dictionary<string, BsonValue>>();
-
-            foreach (var t in tmp){
+            foreach (var t in inventoryList){
                 foreach (var id in t.Elements){
                     if (id.Name == "_id"){
-                        newDic[id.Value] = new Dictionary<string, BsonValue>();
+                        idsDictionary[id.Value] = new Dictionary<string, BsonValue>();
                         foreach (var tElement in t.Elements){
-                            newDic[id.Value][tElement.Name] = tElement.Value;
+                            idsDictionary[id.Value][tElement.Name] = tElement.Value;
                         }
                     }
                 }
             }
 
-            foreach (var (key, value) in newDic){
-                if (value["bikesAvailable"] > 0)
-                    Console.WriteLine($"Bikes available at- {value["name"]}: {value["bikesAvailable"]}");
+            foreach (var (key, value) in idsDictionary){
+                if (value["bikesAvailable"] > 0 && location == value["name"])
+                    Console.WriteLine($"Mongo Db____Bikes available at {value["name"]}: {value["bikesAvailable"]}");
             }
         }
     }
