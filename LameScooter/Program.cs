11e2eb416fb96f8dc252b6a5a3c5e3dbc9692b82
Alex <Alex.rynkowski90@gmail.com>
2021-05-06 10:17:53 +0200

    step 2 compelte

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -4,13 +4,20 @@ using System.Threading.Tasks;
 
 //Assignment 3:
 //https://github.com/marczaku/GP20-2021-0426-Rest-Gameserver/blob/main/assignments/assignment3.md
-namespace LameScooter
-{
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
+namespace LameScooter{
+    class Program{
+        static async Task Main(string[] args){
+            ILameScooterRental lameScooterRental = new LameScooterRental();
+
+            var count = await lameScooterRental.GetScooterCountInStation(null);
+            Console.WriteLine($"Number of Scooter available at this station: {count}");
             Console.WriteLine(args[0]);
+        }
+    }
+
+    public class LameScooterRental : ILameScooterRental{
+        public Task<int> GetScooterCountInStation(string stationName){
+            throw new NotImplementedException();
         }
     }
 }
