2b362fca420a89221baa46fb952bf7c08b1e6a1a
Alex <Alex.rynkowski90@gmail.com>
2021-05-06 09:32:48 +0200

    lamescooter project

---
+++ LameScooter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.Json;
+
+namespace LameScooter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
