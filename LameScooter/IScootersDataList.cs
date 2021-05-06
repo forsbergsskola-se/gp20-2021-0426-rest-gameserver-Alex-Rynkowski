5c906a569fbd38d320bc92b9db04514521ac934d
Alex <Alex.rynkowski90@gmail.com>
2021-05-06 11:03:33 +0200

    implement step 3 (get number of available scooters)

---
+++ LameScooter/IScootersDataList.cs
@@ -0,0 +1,16 @@
+﻿namespace LameScooter{
+    public interface IScootersDataList{
+        string Id{ get; set; }
+        string Name{ get; set; }
+        float X{ get; set; }
+        float Y{ get; set; }
+        int BikesAvailable{ get; set; }
+        int SpacesAvailable{ get; set; }
+        int Capacity{ get; set; }
+        bool AllowDropoff{ get; set; }
+        bool AllowOverloading{ get; set; }
+        bool IsFloatingBike{ get; set; }
+        bool IsCarStation{ get; set; }
+        string State{ get; set; }
+    }
+}
--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 //Assignment 3:
@@ -7,17 +11,37 @@ using System.Threading.Tasks;
 namespace LameScooter{
     class Program{
         static async Task Main(string[] args){
-            ILameScooterRental lameScooterRental = new LameScooterRental();
-
-            var count = await lameScooterRental.GetScooterCountInStation(null);
-            Console.WriteLine($"Number of Scooter available at this station: {count}");
-            Console.WriteLine(args[0]);
+            ILameScooterRental lameScooterRental = new OfflineLameScooterRental();
+            var count = await lameScooterRental.GetScooterCountInStation(args[0]);
+            Console.WriteLine($"Number of available scooters in {args[0]}: {count}");
         }
     }
 
-    public class LameScooterRental : ILameScooterRental{
-        public Task<int> GetScooterCountInStation(string stationName){
-            throw new NotImplementedException();
+    public class OfflineLameScooterRental : ILameScooterRental{
+        async Task<IEnumerable<ScootersesDataList>> ScooterDataToList(){
+            var jsonFile = await File.ReadAllTextAsync("scooter.json");
+            return JsonSerializer.Deserialize<List<ScootersesDataList>>(jsonFile);
         }
+
+        public async Task<int> GetScooterCountInStation(string stationName){
+            return (from jsonData in await ScooterDataToList()
+                where stationName == jsonData.Name
+                select jsonData.BikesAvailable).Sum();
+        }
+    }
+
+    public class ScootersesDataList : IScootersDataList{
+        [JsonPropertyName("id")] public string Id{ get; set; }
+        [JsonPropertyName("name")] public string Name{ get; set; }
+        [JsonPropertyName("x")] public float X{ get; set; }
+        [JsonPropertyName("y")] public float Y{ get; set; }
+        [JsonPropertyName("bikesAvailable")] public int BikesAvailable{ get; set; }
+        [JsonPropertyName("spacesAvailable")] public int SpacesAvailable{ get; set; }
+        [JsonPropertyName("capacity")] public int Capacity{ get; set; }
+        [JsonPropertyName("allowDropoff")] public bool AllowDropoff{ get; set; }
+        [JsonPropertyName("allowOverloading")] public bool AllowOverloading{ get; set; }
+        [JsonPropertyName("isFloatingBike")] public bool IsFloatingBike{ get; set; }
+        [JsonPropertyName("isCarStation")] public bool IsCarStation{ get; set; }
+        [JsonPropertyName("state")] public string State{ get; set; }
     }
 }
