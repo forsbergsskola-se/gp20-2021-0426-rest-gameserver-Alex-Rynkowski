6962df3fa212401358cc8e193335a8062d8990dc
Alex <Alex.rynkowski90@gmail.com>
2021-05-16 00:42:33 +0200

    adds player and item unit test

---
+++ MMOClient/MMO.Test/ItemTests/CreateItemTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Client.Model;
+using Client.Utilities;
+using NUnit.Framework;
+
+namespace MMO.Test.ItemTests{
+    public class CreateItemTests{
+        const string P1 = "Alex R";
+        const string P2 = "Marc Z";
+        const string P3 = "Raphael A";
+
+        Player player1;
+        Player player2;
+        Player player3;
+
+        [Test]
+        public async Task PlayerOneTest(){
+            await SetupTest();
+            var sword = await Item.Get(this.player1.Id, "Holy Sword");
+            var shield = await Item.Get(this.player1.Id, "Holy Shield");
+            Assert.IsNotNull(sword);
+            Assert.IsNull(shield);
+        }
+
+        [Test]
+        public async Task PlayerTwoTest(){
+            await SetupTest();
+            var sword = await Item.Get(this.player2.Id, "Holy Sword");
+            var shield = await Item.Get(this.player2.Id, "Holy Shield");
+            Assert.IsNotNull(shield);
+            Assert.IsNull(sword);
+        }
+
+        [Test]
+        public async Task PlayerThreeTest(){
+            await SetupTest();
+            Assert.IsNull(this.player3.InventoryList);
+        }
+
+        [Test]
+        public async Task GetInventoryPlayerOne(){
+            await SetupTest();
+            var playerOneInventory = await Item.GetAll(this.player1.Id);
+
+            Assert.AreEqual(2, playerOneInventory.Count);
+            Assert.AreEqual(ItemTypes.Armor, playerOneInventory.First(x => x.ItemType == ItemTypes.Armor).ItemType);
+            Assert.AreEqual(20, playerOneInventory.First(x => x.SellValue == 20).SellValue);
+        }
+
+        [Test]
+        public async Task GetInventoryPlayerTwo(){
+            await SetupTest();
+            var playerOneInventory = await Item.GetAll(this.player1.Id);
+            var playerTwoInventory = await Item.GetAll(this.player2.Id);
+
+            Assert.Greater(playerTwoInventory.Count, playerOneInventory.Count);
+            Assert.AreEqual(6, playerTwoInventory.First(x => x.ItemName == "Holy Shield").LevelRequirement);
+            Assert.Greater(playerTwoInventory.First(x => x.ItemName == "Dark Armor").SellValue, 19000);
+        }
+
+        [Test]
+        public async Task PlayerOneSellItemTest(){
+            await SetupTest();
+            Assert.AreEqual(0, this.player1.Gold);
+            await Item.Sell(this.player1.Id, "Holy Sword");
+            this.player1 = await Player.Get(P1);
+            Assert.AreEqual(1000, this.player1.Gold);
+
+        }
+
+        public async Task SetupTest(){
+            await ApiConnection.DeleteRequest<Player>("drop/playerCollection");
+            this.player1 = await Player.Create(P1);
+            this.player2 = await Player.Create(P2);
+            this.player3 = await Player.Create(P3);
+
+            await ItemCreator(this.player1.Id, "Holy Sword", ItemTypes.Sword, ItemRarity.Rare, 3, 2, 1000);
+            await ItemCreator(this.player1.Id, "Basic Armor", ItemTypes.Armor, ItemRarity.Common, 1, 0, 20);
+            await ItemCreator(this.player2.Id, "Holy Shield", ItemTypes.Shield, ItemRarity.Epic, 6, 4, 2000);
+            await ItemCreator(this.player2.Id, "Dark Sword", ItemTypes.Sword, ItemRarity.Common, 1, 4, 2000);
+            await ItemCreator(this.player2.Id, "Dark Armor", ItemTypes.Armor, ItemRarity.Epic, 78, 15, 20000);
+
+
+            this.player1 = await Player.Get(P1);
+            this.player2 = await Player.Get(P2);
+            this.player3 = await Player.Get(P3);
+        }
+
+        static async Task ItemCreator(Guid playerId, string name, ItemTypes type, ItemRarity rarity,
+            int levelRequirement, int levelBonus, int sellValue){
+            await Item.CreateItem(playerId, new Item{
+                ItemName = name,
+                ItemType = type,
+                LevelRequirement = levelRequirement,
+                LevelBonus = levelBonus,
+                Rarity = rarity,
+                SellValue = sellValue
+            });
+        }
+    }
+}
--- MMOClient/Model/Player.cs
+++ MMOClient/Model/Player.cs
@@ -8,6 +8,7 @@ namespace Client.Model{
     public class Player{
         public Guid Id{ get; set; }
         public string Name{ get; set; }
+        public int Gold{ get; set; }
         public int Score{ get; set; }
         public int Level{ get; set; }
         public bool IsDeleted{ get; set; }
