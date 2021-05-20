98fcbfa2e67578e4b6c5e4b65d068f1bd6d2f8cf
Alex <Alex.rynkowski90@gmail.com>
2021-05-20 11:37:08 +0200

    fix lame scooter mongo db to be more clean after more experience with it :)

--- LameScooter/MongoDbLameScooterRental.cs
+++ LameScooter/MongoDbLameScooterRental.cs
@@ -1,44 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using MongoDB.Bson;
+﻿using System.Threading.Tasks;
 using MongoDB.Driver;
 
 namespace LameScooter{
     public class MongoDbLameScooterRental : ILameScooterRental{
-        static async Task<Dictionary<BsonValue, Dictionary<string, BsonValue>>> MongoDb(){
-            var client =
-                new MongoClient("mongodb://localhost:27017");
-
+        static async Task<ScootersDataList> MongoDb(string valueToLookup){
+            var client = new MongoClient("mongodb://localhost:27017");
             var database = client.GetDatabase("LameScooters");
-            var collection = database.GetCollection<BsonDocument>("inventory");
+            var collection = database.GetCollection<ScootersDataList>("inventory");
 
-            var inventoryList = await collection.Find(new BsonDocument()).ToListAsync();
-            var idsDictionary = new Dictionary<BsonValue, Dictionary<string, BsonValue>>();
-
-            foreach (var t in inventoryList){
-                foreach (var id in t.Elements){
-                    if (id.Name != "_id") continue;
-                    idsDictionary[id.Value] = new Dictionary<string, BsonValue>();
-                    foreach (var tElement in t.Elements){
-                        idsDictionary[id.Value][tElement.Name] = tElement.Value;
-                    }
-                }
-            }
-
-
-            return idsDictionary;
+            return await collection.Find(x => x.Name == valueToLookup).FirstAsync();
         }
 
         public async Task<int> GetScooterCountInStation(string stationName){
-            var lst = await MongoDb();
-            BsonValue str = 0;
-            foreach (var (key, value) in lst){
-                if (value["bikesAvailable"] > 0 && stationName == value["name"])
-                    str = value["bikesAvailable"];
-            }
-
-            return Convert.ToInt32(str);
+            var station = await MongoDb(stationName);
+            return station.BikesAvailable;
         }
     }
 }
--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using MongoDB.Driver;
+using Newtonsoft.Json;
 
 //Assignment 3:
 //https://github.com/marczaku/GP20-2021-0426-Rest-Gameserver/blob/main/assignments/assignment3.md
 namespace LameScooter{
     class Program{
         static async Task Main(string[] args){
+            //await ExportStationToMongo();
             try{
                 switch (args[1]){
                     case "offline":
@@ -38,6 +42,25 @@ namespace LameScooter{
                 Console.WriteLine("Argument 2 has to be: \"offline\", \"deprecated\" or \"realtime\"");
                 throw;
             }
+        }
+
+        static async Task ExportStationToMongo(){
+            var client =
+                new MongoClient("mongodb://localhost:27017");
+
+            var database = client.GetDatabase("LameScooters");
+            var collection = database.GetCollection<ScootersDataList>("inventory");
+            var lst = await ScooterDataList();
+            await collection.InsertManyAsync(lst.Stations);
+        }
+
+        static async Task<StationsList> ScooterDataList(){
+            var client = new HttpClient{
+                BaseAddress = new Uri(
+                    "https://raw.githubusercontent.com/marczaku/GP20-2021-0426-Rest-Gameserver/main/assignments/scooters.json")
+            };
+            var response = await client.GetStringAsync(client.BaseAddress);
+            return JsonConvert.DeserializeObject<StationsList>(response);
         }
     }
 }
